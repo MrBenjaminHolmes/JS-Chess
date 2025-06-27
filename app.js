e4e1916170560c358d4224ec2d4e59eb04910479
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2025-06-27 14:36:13 +0100

    Piece Colour

--- app.js
+++ app.js
@@ -18,15 +18,25 @@ const setup = [
 function createBoard() {
   setup.forEach((piece, i) => {
     const square = document.createElement("div");
+    const row = Math.floor((63 - i) / 8) + 1;
+
     square.classList.add("square");
     square.innerHTML = piece;
+
     square.setAttribute("square-id", i);
     board.append(square);
-    const row = Math.floor((63 - i) / 8) + 1;
+
     if (row % 2 === 0) {
       square.classList.add(i % 2 === 0 ? "light" : "dark");
     } else {
       square.classList.add(i % 2 === 0 ? "dark" : "light");
+    }
+    console.log(square.children[0]);
+    if (i <= 15) {
+      square.children[0].classList.add("black");
+    }
+    if (i >= 48) {
+      square.children[0].classList.add("white");
     }
   });
 }
